25bd89aa390bc747db580eccb7bc21abb446c067
mluciano <maritzagarciaso@gmail.com>
2020-11-13 11:01:59 -0400

    create board structure

---
+++ checkers.py
@@ -0,0 +1,25 @@
+class Board(object):
+    
+    def __init__(self): 
+        self.matrix = [['x',' ','x',' ','x',' ','x',' '],
+            [' ','x',' ','x',' ','x',' ','x'],
+            ['x',' ','x',' ','x',' ','x',' '],
+            [' ','x',' ','x',' ','x',' ','x'],
+            ['x',' ','x',' ','x',' ','x',' '],
+            [' ','x',' ','x',' ','x',' ','x'],
+            ['x',' ','x',' ','x',' ','x',' '],
+            [' ','x',' ','x',' ','x',' ','x']]
+
+
+    def update_board(self):
+        pass
+
+
+
+
+
+
+
+
+board = Board()
+print(board.matrix)
