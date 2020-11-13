358c1a17b86133f8210dd1d37b80f574f02e66d0
mluciano <maritzagarciaso@gmail.com>
2020-11-13 13:45:15 -0400

    create player characteristics

--- checkers.py
+++ checkers.py
@@ -29,12 +29,18 @@ class Token(CharacterToken):
     def positioninboard(self):  
         pass      
 
-
+class Player(object):
+    def __init__(self,name,points,cantToken):
+        self.name = name
+        self.points = points
+        self.cantToken = cantToken
 
 
 
 
 token1 = Token("white","0",1,Queen = False)
 token2 = Token("Black","O",1,Queen = False)
+player1 = Player('jugador1',0,12)
 board = Board()
 print(board.matrix)
+print(player1.points)
