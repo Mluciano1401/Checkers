ae93c5d8f52a5ab206f34d2e06ad886fab263ff4
mluciano <maritzagarciaso@gmail.com>
2020-11-13 13:10:19 -0400

    Create token features

--- checkers.py
+++ checkers.py
@@ -14,12 +14,27 @@ class Board(object):
     def update_board(self):
         pass
 
+class CharacterToken(object):
+    def __init__(self,name,simbol):
+        self.name = name
+        self.simbol = simbol
+
+class Token(CharacterToken):
+    def __init__(self,name,simbol,value,Queen = False):
+        CharacterToken.__init__(self,name,simbol)
+        self.value = value
+        if Queen == True:
+            self.value = 2
+
+    def positioninboard(self):  
+        pass      
 
 
 
 
 
 
-
+token1 = Token("white","0",1,Queen = False)
+token2 = Token("Black","O",1,Queen = False)
 board = Board()
 print(board.matrix)
