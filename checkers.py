716181f3b8284575843a4ee416432f74855e6df6
mluciano <maritzagarciaso@gmail.com>
2020-11-13 21:38:06 -0400

    create function for starting position of tokens

--- checkers.py
+++ checkers.py
@@ -15,25 +15,48 @@ class Board(object):
         pass
 
 class Token(object):
-    def __init__(self,name,simbol,value,Queen = False):
+    def __init__(self,name,simbol,value,Queen):
         self.name = name
         self.simbol = simbol
         self.value = value
+        self.Queen = Queen
         if Queen == True:
             self.value = 2
+           
+class Token_Initialposition(Token):
+    def __init__(self,name,simbol,value,Queen,matrix):
+        Token.__init__(self,name,simbol,value,Queen = False)
+        self.matrix = matrix
+        if self.name == "Black":
+            for x in range(0,8):
+                for y in range(0,3):
+                    if ' ' == matrix[y][x]:
+                        matrix[y][x] = self.simbol 
+        if self.name == "White":
+            for x in range(0,8):
+                for y in range(5,8): 
+                    if ' ' == matrix[y][x]:
+                        matrix[y][x] = self.simbol
+                        
 
+          
 class Player(object):
     def __init__(self,name,points,canttokens):
         self.name = name
         self.points = points
         self.canttokens = canttokens
+        
+
+
 
 board = Board()
-print(board.matrix)
-token1 = Token("white","0",1,Queen = False)
-token2 = Token("Black","O",1,Queen = False)
+token1 = Token("white","0",1,False)
+token2 = Token("Black","O",1,False)
 player1 = Player('Jugador1',0,12)
 player2 = Player('Jugador2',0,12)
+boardinitial = Token_Initialposition("Black","O",1, False,board.matrix)
+boardinitial1 = Token_Initialposition("White","0",1, False,board.matrix)
+print(boardinitial1.matrix)
 print(player1.__dict__)
 print(token1.__dict__)
 
