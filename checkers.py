0cc6cb7cede5f41ab1ed7622597b7cf75d4d66ca
mluciano <maritzagarciaso@gmail.com>
2020-11-15 20:33:20 -0400

    follow-up in the improvement of the code

--- checkers.py
+++ checkers.py
@@ -15,15 +15,16 @@ class Board(object):
         pass
  
 class Player(object):
-    def __init__(self,name,points,canttokens):
+    def __init__(self,name,points,canttokens,colortoken):
         self.name = name
         self.points = points
         self.canttokens = canttokens
+        self.colortoken = colortoken
     
  
 
 class Token(object):
-    def __init__(self,name,simbol,value,Queen):
+    def __init__(self,name,simbol,value,Queen,x,y):
         self.name = name
         self.simbol = simbol
         self.value = value
@@ -31,33 +32,37 @@ class Token(object):
         if Queen == True:
             self.value = 2
             self.simbol = 'Q'
+        self.x = x 
+        self.y = y    
+
+    def token(self):
+        return self.simbol        
 
            
-class Token_Initialposition(Token):
+def Token_Initialposition(token,matrix):
     
-    token1 = Token("white","0",1,False)
-    token2 = Token("Black","O",1,False)
-    def __init__(self,name,simbol,value,Queen,matrix):
-        Token.__init__(self,name,simbol,value,Queen = False)
-        self.matrix = matrix
-        if self.name == "Black":
-            for x in range(0,8):
-                for y in range(0,3):
-                    if ' ' == matrix[y][x]:
-                        matrix[y][x] = self.simbol 
-        if self.name == "White":
-            for x in range(0,8):
-                for y in range(5,8): 
-                    if ' ' == matrix[y][x]:
-                        matrix[y][x] = self.simbol
+    if token.name == "Black":
+        for x in range(0,8):
+            for y in range(0,3):
+                if ' ' == matrix[y][x]:
+                    matrix[y][x] = token.token()
+    if token.name == "White":
+        for x in range(0,8):
+            for y in range(5,8): 
+                if ' ' == matrix[y][x]:
+                    matrix[y][x] = token.token()
 
+    return matrix
 
 board = Board()
-player1 = Player('Player1',0,12)
-player2 = Player('Player2',0,12)
-boardinitial = Token_Initialposition("Black","O",1, False,board.matrix)
-boardinitial = Token_Initialposition("White","0",1, False,board.matrix)
-for e in boardinitial.matrix:
+token1 = Token("White","0",1,False,0,0)
+token2 = Token("Black","O",1,False,0,0)
+    
+player1 = Player('Player1',0,12,"Black")
+player2 = Player('Player2',0,12,"White")
+boardinitial = Token_Initialposition(token1,board.matrix)
+boardinitial = Token_Initialposition(token2,board.matrix)
+for e in boardinitial:
     print(e) 
 print(player1.__dict__)
 print(player2.__dict__)
