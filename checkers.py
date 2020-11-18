8bf16bb009a23f22413e1ad072f0983859e40a1f
mluciano <maritzagarciaso@gmail.com>
2020-11-18 10:00:26 -0400

    create the characteristics of the shifts

--- checkers.py
+++ checkers.py
@@ -40,7 +40,7 @@ class Token(object):
    
 
 class Gameplayfeatures(object):
-    def __init__(self,xposition,yposition):
+    def __init__(self):
         self.xposition = xposition
         self.yposition = yposition
 
@@ -69,7 +69,7 @@ class Gameplayfeatures(object):
         return matrix
 
     @staticmethod 
-    def space_blank(player,matrix):
+    def space_blank(player,matrix,target):
         
         availabletoken = {}
         for e in player.positiontokens:
@@ -78,21 +78,21 @@ class Gameplayfeatures(object):
             x= e[1]
             if x < 7 and y < 7 and x > 0 and y > 0:
                 if player.colortoken == "Black":
-                    if matrix[y + 1][x + 1] == ' ':
+                    if matrix[y + 1][x + 1] == target:
                         z = [y,x]
                         z = str(z)
                         newpositiontokens.append([y + 1,x + 1])
                         availabletoken[z] = newpositiontokens
-                    if matrix[y + 1][x - 1] == ' ':
+                    if matrix[y + 1][x - 1] == target:
                         newpositiontokens.append([y + 1, x - 1])
                         availabletoken[z] =  newpositiontokens   
                 if player.colortoken == "White":
-                    if matrix[y - 1][x + 1] == ' ':
+                    if matrix[y - 1][x + 1] == target:
                         v = [y,x]
                         v = str(v)
                         newpositiontokens.append([y - 1, x + 1])
                         availabletoken[v] = newpositiontokens
-                    if matrix[y - 1][x - 1] == ' ':
+                    if matrix[y - 1][x - 1] == target:
                         newpositiontokens.append([y - 1, x - 1]) 
                         availabletoken[v] =  newpositiontokens        
         player.positiontokens = availabletoken  
@@ -105,7 +105,7 @@ class Gameplayfeatures(object):
         c = [i,j]
         z = str(c)
         if z in player.positiontokens:
-            print(player.positiontokens[z])
+            print('posible movimientos -->', player.positiontokens[z])
         coordinates = input('insert the coordinates: (example -> 0,1) ')
         y,x = coordinates
         if player.colortoken == "Black":
@@ -113,9 +113,9 @@ class Gameplayfeatures(object):
         if player.colortoken == "White":
             matrix[y][x] = 'W'    
         
-    
-    def winner(self,point):
-        if self.point >= 12:
+    @staticmethod 
+    def winner(point):
+        if point >= 12:
             return True
         else:
             return False 
@@ -123,31 +123,31 @@ class Gameplayfeatures(object):
 
     def deletetoken():
         pass  
-   
-    
-def rotatingshifts():      
-    board = Board()   
-    player1 = Player(0,12,"Black",[])
-    player2 = Player(0,12,"White",[])
-    boardinitial = Gameplayfeatures.Token_Initialposition(player1,board.matrix)
-    boardinitial = Gameplayfeatures.Token_Initialposition(player2,board.matrix)
 
-    for e in boardinitial:
-        print(e)   
-    print('total tokens on the board -->',Token.numtokens)
-    print(player1.__dict__)
-    print(player2.__dict__)
-    spaceblank = Gameplayfeatures.space_blank(player1,boardinitial)
-    spaceblank = Gameplayfeatures.space_blank(player2,boardinitial)
-    print(player1.__dict__)
-    print(player2.__dict__)
-    move =  Gameplayfeatures.movetoken(player1,boardinitial)
     
-    for e in boardinitial:
-        print(e)   
-    return
-
-rotatingshifts()
+def rotatingshifts(players,boardinitial):      
+    while True:
+        for player in players:
+            print('turn of ->', player.name)
+            if Gameplayfeatures.winner(player.points):
+                return "Winner ->", player.name
+                break
+            else:
+                for e in boardinitial:
+                    print(e)   
+                print('total tokens on the board -->',Token.numtokens)
+                spaceblank = Gameplayfeatures.space_blank(player,boardinitial, ' ')
+                print(player.__dict__)
+                move =  Gameplayfeatures.movetoken(player,boardinitial)
+                player.points += 1 
+           
+board = Board()   
+player1 = Player(0,12,"Black",[])
+player2 = Player(0,12,"White",[])
+boardinitial = Gameplayfeatures.Token_Initialposition(player1,board.matrix)
+boardinitial = Gameplayfeatures.Token_Initialposition(player2,board.matrix)    
+players = [player1,player2]
+rotatingshifts(players,boardinitial)
 
 
 
